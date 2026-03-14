7d8faacd3aad17ba060b12c9af7b3557245cf9f0
chaudhariamol23 <chaudhariamol23@gmail.com>
2026-03-14 16:50:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,19 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/pwa-starter/",
+  "/pwa-starter/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('Fetching:', event.request.url);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
